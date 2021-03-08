7ba5257e66ea947c72f1703888ab1963f5957382
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2021-03-08 17:21:44 -0300

    Adicionando o receitas_app nas configurações do projeto

--- venv/receitas/settings.py
+++ venv/receitas/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'receitas_app', # registrando o app receitas_app no projeto receitas
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
