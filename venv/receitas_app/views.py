46fdadaddc55b6bf2b3e7bcc3bec1fc25d26831b
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2021-03-09 16:59:57 -0300

    Gerando receitas de forma dinâmica

--- venv/receitas_app/views.py
+++ venv/receitas_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1: 'Lasanha', 
+        2: 'Sopa de legumes', 
+        3: 'Sorvete',
+        4: 'Palha jamaicana'
+        }
+    dados = {'nome_das_receitas': receitas}
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
