da91ff07c332c0e3677f5f764bf0d3e3f28830d2
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2021-03-09 22:25:38 -0300

    Registrando admin ao django

--- venv/receitas/settings.py
+++ venv/receitas/settings.py
@@ -66,12 +66,12 @@ WSGI_APPLICATION = 'receitas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.mysql',
-        'NAME':     'django_receitas',
+        'NAME':     'django_receitas', # nome do schema
         'USER':     'root',
         'PASSWORD': 'root',
         'HOST':     'localhost',   # Or an IP Address that your DB is hosted on
         'PORT':     '3306',
-        'DATABASE': 'bd_django'
+        'DATABASE': 'bd_django' # nome do BD
     }
 }
 
