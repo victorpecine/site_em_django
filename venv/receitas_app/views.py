c58de53241f332b723b4c8bec05f8157bb827958
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2021-03-15 18:21:02 -0300

    Criando a página de busca
    Criado uma rota para a busca e exibindo os resultados da busca.

--- venv/receitas_app/views.py
+++ venv/receitas_app/views.py
@@ -11,3 +11,13 @@ def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id) # obtendo a receita a partir do index no BD
     receita_a_exibir = {'receita': receita}
     return render(request, 'receita.html', receita_a_exibir)
+
+# configurando a busca da index
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(receita_publicada=True)
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+    dados = {'receitas': lista_receitas}
+    return render(request, 'buscar.html', dados)
