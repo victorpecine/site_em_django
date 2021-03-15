289dac201a13e35cfc679171f09d169f32c78eae
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2021-03-15 16:01:39 -0300

    Listando as receitas por nome

--- venv/receitas_app/admin.py
+++ venv/receitas_app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita) # registra o app de receitas
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria')
+    list_display_links = ('id', 'nome_receita')
+
+# registra o modelo do app de receitas
+admin.site.register(Receita, ListandoReceitas)
