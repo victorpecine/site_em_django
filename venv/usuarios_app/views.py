058954a0c41ff9218e130fd6cffa3cacda3f6f12
Victor Pecine <victorpecine@gmail.com>
2021-03-16 10:23:41 -0300

    Corrigindo possíveis erros de cadastro
    Campos em branco, e-mail já cadastrado, senha e confirmação de senha diferentes.

--- venv/usuarios_app/views.py
+++ venv/usuarios_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == 'POST':
@@ -6,6 +7,22 @@ def cadastro(request):
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome não pode ficar me branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo e-mail não pode ficar me branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas digitadas não são iguais')
+            return redirect('cadastro')
+        # verificando pelo e-mail se o usuário já está no banco de dados
+        if User.objects.filter(email=email).exists():
+            print('Usuário já cadastrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário cadastrado com sucesso')
         print(nome, email, senha, senha2)
         return redirect('login')
     else:
