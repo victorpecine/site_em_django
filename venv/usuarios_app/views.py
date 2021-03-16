c34f659f0d727e5e49d54ff5f86829d2d94aa47a
Victor Pecine <victorpecine@gmail.com>
2021-03-16 14:51:22 -0300

    Criando formulário para postar recitas
    Atuando com os dados da requisição POST.
    Salvando as receitas no banco de dados.

--- venv/usuarios_app/views.py
+++ venv/usuarios_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User, auth
-# from django.contrib import auth
+from receitas_app.models import Receita
 
 def cadastro(request):
     if request.method == 'POST':
@@ -54,6 +54,36 @@ def logout(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, 'usuarios/dashboard.html')
+        id = request.user.id
+        # mostrando apenas as receitas do autor(user) filtradas pelo respectivo id
+        receitas = Receita.objects.order_by('-data_receita').filter(autor=id)
+        # passando as informações(dados) para o template
+        dados = {'receitas': receitas}
+        return render(request, 'usuarios/dashboard.html', dados)
     else:
         return redirect('index')
+
+def cria_receita(request):
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        # requisição passando o id do autor para user
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(autor=user, 
+                                        nome_receita=nome_receita, 
+                                        ingredientes=ingredientes, 
+                                        modo_de_preparo=modo_preparo, 
+                                        tempo_de_preparo=tempo_preparo, 
+                                        rendimento=rendimento, 
+                                        categoria=categoria, 
+                                        foto_receita=foto_receita)
+        # salvando a receita no banco de dados
+        receita.save()
+        return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/cria_receita.html')
